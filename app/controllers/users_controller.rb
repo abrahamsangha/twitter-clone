0edb33bbe46ded8fe73cc89a31d7c4cd9720ec6c
abrahamsangha <abraham.sangha@gmail.com>
2016-01-18 22:34:01 -0600

    Add login functionality

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      flash.now[:danger] = "C'mon, now..."
+      render 'new'
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name, :last_name, :password_confirmation, :password, :handle)
+  end
+end
